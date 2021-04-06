a00ecb670575a1c12dce558c5c4bb479691bcb15
Evgeniy-cmd <evgermol1732@gmail.com>
2021-04-06 17:06:13 +0300

    edit responce

--- methods/post.task.js
+++ methods/post.task.js
@@ -1,38 +1,47 @@
-const fs = require('file-system')
-const express = require('express')
-const Router = express.Router()
-const jsonParser = express.json()
 const filePath = 'tasks.json'
 const { v4: uuidv4 } = require('uuid')
-
+const fs = require('file-system')
+const express = require('express')
 const { body, validationResult } = require('express-validator')
 
-const router = Router.post('/', jsonParser, body('name').optional().isString(),
-body('done').optional().isBoolean(), (req, res) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).send('Invalid field in request');
-    }
+const Router = express.Router()
 
-    const el = {
-        uuid: uuidv4(),
-        createdAt: new Date(),
-        done: false,
-        name: req.body.name
-    }
 
-    if(fs.existsSync(filePath)){
-      const content = fs.readFileSync(filePath, 'utf8')
-      const tasks = JSON.parse(content)
-      const newTask = tasks.push(el)
-      fs.writeFileSync(filePath, JSON.stringify(tasks))
-      res.send(newTask)
-    } else {
-      fs.appendFile(filePath, JSON.stringify([newTask]))
-      res.send(newTask)
-    }
+const router = Router.post('/', body('name').isString(), function(req,res){
+    if(!req.body.name) return res.status(400).send("Dont name task")
+    
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() })
+        }
+
+        const newTask = {
+            uuid: uuidv4(), 
+            date: new Date(), 
+            done: false, 
+            name: req.body.name
+          }
+
+        if(fs.existsSync(filePath)){
+            const content  = fs.readFileSync(filePath, 'utf8')     
+            const tasks = JSON.parse(content)
+            console.log(content)
+            tasks.push(newTask)
+
+            fs.writeFileSync(filePath, JSON.stringify(tasks), err => {
+                if(err){
+                     res.send(err)
+                }
+            }) 
+            res.send(newTask)  
+        }
+        else {
+            fs.appendFile(filePath, JSON.stringify([newTask]), function (err) {
+                if (err) throw err
+                res.send(newTask) 
+            })
+        }
 })
-
 
 module.exports = router
