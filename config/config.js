dc983d04daf066b704fa204d59686f8110b6495c
Evgeniy-cmd <evgermol1732@gmail.com>
2021-04-12 13:24:24 +0300

    fix config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-{
+module.exports = {
+  [process.env.NODE_ENV || 'development']: {
   "development": {
     "username": "xtolfiswmuypvu",
     "password": "80604ca44548c7dc9cb085ab0050d799510802165243c2f61f9720090b0142e1",
@@ -6,6 +7,6 @@
     "host": "ec2-3-234-85-177.compute-1.amazonaws.com",
     "dialect": "postgres",
     "port": 5432,
-    "url": "postgres://xtolfiswmuypvu:80604ca44548c7dc9cb085ab0050d799510802165243c2f61f9720090b0142e1@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d8srm87upt5q9u"
+    "url": process.env.DATABASE_URL
   }
-}
+}}
