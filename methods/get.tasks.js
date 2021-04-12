11d1d76bf892dfe78545fa84777baa697c0b4683
Evgeniy-cmd <evgermol1732@gmail.com>
2021-04-12 16:20:19 +0300

    fix get request

--- methods/get.tasks.js
+++ methods/get.tasks.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Router = express.Router()
 const { Task } = require('../models')
+const limitOfTasks = 5
 
 
 const router = Router.get('/task',
@@ -8,10 +9,10 @@ const router = Router.get('/task',
         const param = {
             where: {},
             order: [],
-            // offset: req.query.page * 5,
-            limit: 5
+            offset: (req.query.page - 1) * limitOfTasks,
+            limit: limitOfTasks
         }
-        
+        console.log(req.query)
         if (req.query.done)
             param.where = { done: req.query.done }
         if (req.query.order)
