895b20585537d3a54daa6bdef492bb4b388d78c2
Evgeniy-cmd <evgermol1732@gmail.com>
2021-04-15 12:45:59 +0300

    change config path

--- models/index.js
+++ models/index.js
@@ -5,13 +5,11 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../../config/config.json')[env];
+const config = require(__dirname + '../../config/config.js')[env];
 const db = {};
 
 
 const url = process.env.DATABASE_URL
-
-console.log(url)
 
 const sequelize = new Sequelize(url, config);
 
